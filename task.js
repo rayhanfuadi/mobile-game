7accb17f80204fd5834343873098509377237794
rayhanfuadi <oke.rayhan1@gmail.com>
2025-01-04 15:43:01 +0700

    first commit

---
+++ task.js
@@ -0,0 +1,29 @@
+// NetJs FrontEnd Challenge PT. Andara Rejo Makmur
+
+// (harap gunakan NextJs untuk Tugas ini)
+
+// Detail: Anda ditugaskan untuk membuat halaman situs web Landing untuk game seluler yang memungkinkan pemain untuk mengirimkan dan mengedit skor tertinggi mereka dan melihat papan peringkat.
+
+// TUGAS
+// 1. Membuat halaman untuk menampilkan Papan Peringkat skor, dengan data tiruan/semu
+// 2. Skor papan peringkat dapat diurutkan berdasarkan skor menurun, juga dapat difilter berdasarkan Skor, nama (pencarian)
+// 3. Pemain dapat membagikan skor mereka melalui media sosial, yaitu WhatsApp, Telegram, Instagram, Facebook
+// 4. Pemain dapat mengedit nama, foto, skor mereka di Papan Peringkat melalui titik akhir
+// Spesifikasi Teknis:
+// 1. Menggunakan data tiruan/semu untuk nama, skor, foto:
+// mis.
+// 1. https://www.pinterest.com/thumbnail.jpg
+// 2. https://www.pinterest.com/thumbnail.jpg
+const mockData = [
+    {
+        id: 1, nama: 'John Doe', skor: 69, url_foto: ""
+    },
+    { id: 2, nama: 'Jane Smith', skor: 33, url_foto: "" },
+    // Lebih banyak data tiruan...
+];
+// 2. Gunakan NextJs Framework
+// 3. Untuk skor Submit dan Edit, dapat "console.log" payload dari API submitAPI dan updateAPI, tidak perlu mencapai akhir proses submit dan update
+// 4. Bukti Tugas yang telah diselesaikan:
+// sediakan sebagai Screenshot untuk halaman Leaderboard, dan saat edit sebagai bukti keberhasilan Tugas
+// Pengiriman
+// Email ke muhrahk110 @gmail.com, lalu cc: johandyyahya737 @gmail.com sebelum 06 Januari 2025, 23.59, dengan penjelasan singkat tentang fitur apa yang dibuat.disertakan repo github dan kami berharap dapat segera mendiskusikan tugas Anda dengan Anda.sS
