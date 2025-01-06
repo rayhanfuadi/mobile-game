16358e4508fa073bd6170694d59493dc547c9793
rayhanfuadi <oke.rayhan1@gmail.com>
2025-01-07 01:00:08 +0700

    update ui

--- src/components/layouts/EditPlayersForm.tsx
+++ src/components/layouts/EditPlayersForm.tsx
@@ -1,8 +1,5 @@
 
 interface Props {
-    // value: { id: string; nama: string; skor: number; url_foto: string } | null
-    // onChange: (value: { id: string; nama: string; skor: number; url_foto: string } | null) => void
-    // onClick: () => void
     valueNama: string,
     onChangeNama: (e: React.ChangeEvent<HTMLInputElement>) => void;
     valueSkor: number,
@@ -16,46 +13,74 @@ interface Props {
 const EditPlayersForm: React.FC<Props> = (props: Props) => {
     const { valueNama, onChangeNama, valueSkor, onChangeSkor, valueUrl_foto, onChangeUrl_foto, setEditingUser, onClick } = props
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded shadow-lg w-96">
-                <h2 className="text-xl font-bold mb-4">Edit Player</h2>
-                <input
-                    type="text"
-                    placeholder="Name"
-                    value={valueNama}
-                    onChange={onChangeNama}
-                    className="border p-2 w-full mb-4"
-                />
-                <input
-                    type="number"
-                    placeholder="Score"
-                    value={valueSkor}
-                    min={0}
-                    max={100}
-                    onChange={onChangeSkor}
-                    className="border p-2 w-full mb-4"
-                />
-                <p>Photo URL: (silahkan masukan foto dari link address pinterset)</p>
-                <input
-                    type="text"
-                    placeholder="Photo URL"
-                    value={valueUrl_foto}
-                    onChange={onChangeUrl_foto}
-                    className="border p-2 w-full mb-4"
-                />
-                <div className="flex justify-end">
-                    <button
-                        className="bg-red-500 text-white px-4 py-2 rounded mr-2"
-                        onClick={setEditingUser}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        className="bg-blue-500 text-white px-4 py-2 rounded"
-                        onClick={onClick}
-                    >
-                        Save
-                    </button>
+        <div className="">
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center ">
+                <div className="bg-radial-primary p-6 rounded shadow-lg w-96 drop-shadow-form">
+                    <div className="text-center grid gap-2">
+                        <h2 className="text-xl font-bold">Edit Player</h2>
+                        <p className="text-sm text-[#999] mb-4">Buat perubahan untuk profil anda disini. Klik save ketika kamu sudah selesai.</p>
+                    </div>
+                    <div className="grid gap-3">
+                        <div className="grid grid-cols-12 gap-4 h-full">
+                            <div className="col-span-3">
+                                <p className="flex justify-end  items-center h-full">Nama</p>
+                            </div>
+
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                min={0}
+                                max={100}
+                                value={valueNama}
+                                onChange={onChangeNama}
+                                className="col-span-9 h-full border px-4 py-2 w-full mb-4 text-white bg-transparent rounded-lg focus:outline-none"
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-12 gap-4 h-full">
+                            <div className="col-span-3">
+                                <p className="flex justify-end  items-center h-full">Skor</p>
+                            </div>
+
+                            <input
+                                type="number"
+                                placeholder="Skor"
+                                value={valueSkor}
+                                onChange={onChangeSkor}
+                                className="col-span-9 h-full border px-4 py-2 w-full mb-4 text-white bg-transparent rounded-lg focus:outline-none"
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-12 gap-4 h-full">
+                            <div className="col-span-3">
+                                <p className="flex justify-end  items-center h-full">Url Foto</p>
+                            </div>
+
+                            <input
+                                type="text"
+                                placeholder="Masukan URL"
+                                value={valueUrl_foto}
+                                onChange={onChangeUrl_foto}
+                                className="col-span-9 h-full border px-4 py-2 w-full mb-4 text-white bg-transparent rounded-lg focus:outline-none"
+                            />
+                        </div>
+                        <p className="text-sm text-[#999]">Photo URL: (silahkan masukan foto dari link address pinterset)</p>
+                    </div>
+
+                    <div className="flex justify-end">
+                        <button
+                            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+                            onClick={setEditingUser}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="bg-blue-500 text-white px-4 py-2 rounded"
+                            onClick={onClick}
+                        >
+                            Save
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -64,45 +89,46 @@ const EditPlayersForm: React.FC<Props> = (props: Props) => {
 
 export default EditPlayersForm
 
-
-
-// < div className = "fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" >
-//     <div className="bg-white p-6 rounded shadow-lg w-96">
-//         <h2 className="text-xl font-bold mb-4">Edit Player</h2>
-//         <input
-//             type="text"
-//             placeholder="Name"
-//             value={editingUser.nama}
-//             onChange={(e) => setEditingUser({ ...editingUser, nama: e.target.value })}
-//             className="border p-2 w-full mb-4"
-//         />
-//         <input
-//             type="number"
-//             placeholder="Score"
-//             value={editingUser.skor}
-//             onChange={(e) => setEditingUser({ ...editingUser, skor: +e.target.value })}
-//             className="border p-2 w-full mb-4"
-//         />
-//         <input
-//             type="text"
-//             placeholder="Photo URL"
-//             value={editingUser.url_foto}
-//             onChange={(e) => setEditingUser({ ...editingUser, url_foto: e.target.value })}
-//             className="border p-2 w-full mb-4"
-//         />
-//         <div className="flex justify-end">
-//             <button
-//                 className="bg-red-500 text-white px-4 py-2 rounded mr-2"
-//                 onClick={() => setEditingUser(null)}
-//             >
-//                 Cancel
-//             </button>
-//             <button
-//                 className="bg-blue-500 text-white px-4 py-2 rounded"
-//                 onClick={handleEdit}
-//             >
-//                 Save
-//             </button>
-//         </div>
-//     </div>
-//     </ >
+{/* <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                <div className="bg-radial-primary p-6 rounded shadow-lg w-96">
+                    <h2 className="text-xl font-bold mb-4">Edit Player</h2>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={valueNama}
+                        onChange={onChangeNama}
+                        className="border p-2 w-full mb-4 text-black"
+                    />
+                    <input
+                        type="number"
+                        placeholder="Score"
+                        value={valueSkor}
+                        min={0}
+                        max={100}
+                        onChange={onChangeSkor}
+                        className="border p-2 w-full mb-4 text-black"
+                    />
+                    <p>Photo URL: (silahkan masukan foto dari link address pinterset)</p>
+                    <input
+                        type="text"
+                        placeholder="Photo URL"
+                        value={valueUrl_foto}
+                        onChange={onChangeUrl_foto}
+                        className="border p-2 w-full mb-4 text-black"
+                    />
+                    <div className="flex justify-end">
+                        <button
+                            className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+                            onClick={setEditingUser}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="bg-blue-500 text-white px-4 py-2 rounded"
+                            onClick={onClick}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </div> */}
